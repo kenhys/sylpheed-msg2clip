6fa616ba4f55eea4ceceabdea8e61f1fca78e271
HAYASHI Kentaro <kenhys@gmail.com>
2013-04-06 15:11:44 +0900

    integrate ui related code to main source

--- src/msg2clip.c
+++ src/msg2clip.c
@@ -363,3 +363,102 @@ static void copy_btn_clicked(GtkButton *button, gpointer data)
 }
 
 
+static gchar* g_copyright = N_("Msg2Clip is distributed under GPL license.\n"
+			       "\n"
+			       "Copyright (C) 2012 HAYASHI Kentaro <kenhys@gmail.com>"
+			       "\n"
+			       "msg2clip contains following resource.\n"
+			       "\n"
+			       "page_save.xpm: converted from page_save.png.\n"
+			       "Silk icon set 1.3 By Mark James\n"
+			       "http://www.famfamfam.com/lab/icons/silk/\n"
+			       "Creative Commons Attribution 2.5 License.\n");
+
+GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
+{
+  GtkWidget *vbox;
+  GtkWidget *startup_align;
+  GtkWidget *startup_frm;
+  GtkWidget *startup_frm_align;
+  GtkWidget *general_lbl;
+  
+  debug_print("create_config_main_page\n");
+  if (notebook == NULL){
+    return NULL;
+  }
+  /* startup */
+  if (pkey!=NULL){
+  }
+  vbox = gtk_vbox_new(FALSE, 0);
+
+  /**/
+  startup_align = gtk_alignment_new(0, 0, 1, 1);
+  gtk_alignment_set_padding(GTK_ALIGNMENT(startup_align), ALIGN_TOP, ALIGN_BOTTOM, ALIGN_LEFT, ALIGN_RIGHT);
+
+  startup_frm = gtk_frame_new(_("Startup Option"));
+  startup_frm_align = gtk_alignment_new(0, 0, 1, 1);
+  gtk_alignment_set_padding(GTK_ALIGNMENT(startup_frm_align), ALIGN_TOP, ALIGN_BOTTOM, ALIGN_LEFT, ALIGN_RIGHT);
+
+
+  g_opt.startup = gtk_check_button_new_with_label(_("Enable plugin on startup."));
+  gtk_container_add(GTK_CONTAINER(startup_frm_align), g_opt.startup);
+  gtk_container_add(GTK_CONTAINER(startup_frm), startup_frm_align);
+  gtk_container_add(GTK_CONTAINER(startup_align), startup_frm);
+
+  gtk_widget_show(g_opt.startup);
+
+  /**/
+  gtk_box_pack_start(GTK_BOX(vbox), startup_align, FALSE, FALSE, 0);
+
+  general_lbl = gtk_label_new(_("General"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, general_lbl);
+  gtk_widget_show_all(notebook);
+  return NULL;
+
+}
+
+/* about, copyright tab */
+GtkWidget *create_config_about_page(GtkWidget *notebook, GKeyFile *pkey)
+{
+  GtkWidget *hbox;
+  GtkWidget *vbox;
+  GtkWidget *lbl;
+  GtkWidget *desc;
+  GtkWidget *scrolled;
+  GtkTextBuffer *tbuffer;
+  GtkWidget *tview;
+  GtkWidget *general_lbl;
+  
+  debug_print("create_config_about_page\n");
+  if (notebook == NULL){
+    return NULL;
+  }
+  
+  hbox = gtk_hbox_new(TRUE, 6);
+  vbox = gtk_vbox_new(FALSE, 6);
+
+  lbl = gtk_label_new(_("Msg2Clip"));
+  desc = gtk_label_new(PLUGIN_DESC);
+
+  /* copyright */
+  scrolled = gtk_scrolled_window_new(NULL, NULL);
+
+  gtk_box_pack_start(GTK_BOX(vbox), lbl, FALSE, TRUE, 6);
+  gtk_box_pack_start(GTK_BOX(vbox), desc, FALSE, TRUE, 6);
+  gtk_box_pack_start(GTK_BOX(vbox), scrolled, TRUE, TRUE, 6);
+  gtk_box_pack_start(GTK_BOX(hbox), vbox, TRUE, TRUE, 6);
+
+  tbuffer = gtk_text_buffer_new(NULL);
+  gtk_text_buffer_set_text(tbuffer, _(g_copyright), strlen(g_copyright));
+  tview = gtk_text_view_new_with_buffer(tbuffer);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(tview), FALSE);
+  gtk_container_add(GTK_CONTAINER(scrolled), tview);
+    
+  gtk_box_pack_start(GTK_BOX(vbox), scrolled, TRUE, TRUE, 6);
+    
+  /**/
+  general_lbl = gtk_label_new(_("About"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), hbox, general_lbl);
+  gtk_widget_show_all(notebook);
+  return NULL;
+}
