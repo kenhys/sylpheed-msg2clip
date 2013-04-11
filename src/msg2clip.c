488e27d0ce767c0cc49bd76a0f91e841858cf14c
HAYASHI Kentaro <kenhys@gmail.com>
2013-04-11 22:59:28 +0900

    change license to 2-Clause BSD

--- src/msg2clip.c
+++ src/msg2clip.c
@@ -1,20 +1,6 @@
 /*
  * Msg2Clip -- 
  * Copyright (C) 2012 HAYASHI Kentaro
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include "config.h"
@@ -369,12 +355,6 @@ static gchar* g_copyright = N_("Msg2Clip is distributed under GPL license.\n"
 			       "\n"
 			       "Copyright (C) 2012 HAYASHI Kentaro <kenhys@gmail.com>"
 			       "\n"
-			       "msg2clip contains following resource.\n"
-			       "\n"
-			       "page_save.xpm: converted from page_save.png.\n"
-			       "Silk icon set 1.3 By Mark James\n"
-			       "http://www.famfamfam.com/lab/icons/silk/\n"
-			       "Creative Commons Attribution 2.5 License.\n");
 
 GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
 {
